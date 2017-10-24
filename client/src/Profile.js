37644b77c6f1b7cba6e613e58a5c83d48cf85179
Ari-M <ariminton@live.com>
2017-10-24 15:53:54 -0700

    Axios call to server for Watson API

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -4,6 +4,26 @@ import Entry from './Entry';
 import Response from './Response';
 
 class Profile extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            this: 'that'
+        }
+    }
+
+    //componentDidMount() {
+        /* Here is where a call for the persons profile needs to be made. Once the verification is made the credentials will be stored in the 
+        state. The credentials will be passed down as props to all the other components so that when a component loads the information will 
+        be much faster to show up. */
+    //}
+
+    /* THIS IS FOR THE ENTRY COMPONENT */
+
+
+
+    /* END OF ENTRY COMPONENT */
+
     render(){
         return(
             <div>
--- routes/watson.js
+++ routes/watson.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/isLoggedIn');
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+var submitted;
 
 var tone_analyzer = new ToneAnalyzerV3({
   username: 'a32ea9d2-b998-443b-87b7-6aef61ba95d3',
@@ -11,16 +12,22 @@ var tone_analyzer = new ToneAnalyzerV3({
 
 
 /* This is just a basic hookup to connect to the Watson API  */
-tone_analyzer.tone({ text: 'This is a different text to see if there can be differing tones that can contribute to a more thorough analysis of this certain api.' },
-  function(err, tone) {
-    if (err)
-      console.log(err);
-    else
-      console.log(JSON.stringify(tone, null, 2));
-});
+
 
 router.get('/', function(req, res, next) {
 	res.send('This is the watson page');
+})
+
+router.post('/', function(req, res, next) {
+	console.log(req);
+	
+	tone_analyzer.tone({ text: req.body.entry },
+	  function(err, tone) {
+	    if (err)
+	      console.log(err);
+	    else
+	      console.log(JSON.stringify(tone, null, 2));
+	});
 })
 
 module.exports = router;
