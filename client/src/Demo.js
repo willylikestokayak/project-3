61390a398e1678f7f3f3ee9661bc8c13c7da9067
hsg2007@yahoo.de <YOUR-EMAIL-ADDRESS>
2017-10-26 14:20:43 -0700

    carousel n things

--- client/src/Demo.js
+++ client/src/Demo.js
@@ -10,31 +10,19 @@ class Demo extends Component {
                 <div>
                   <h4>Welcome to WYM.</h4>
                   <p>[Filler copy/not final] WYM uses the Watson tone analyzer API to help you detect tone in your written text. Get started by following the steps below.</p>
-                <Carousel images={[
-'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg',
-'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg',
-'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg',
-'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg',
-'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg'
-]} />
+
+      	             <Carousel options={{ fullWidth: false }} images={[
+                       'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg',
+                       'http://i.dailymail.co.uk/i/pix/2012/02/02/article-2095088-1178BD6E000005DC-983_468x560.jpg',
+                       'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg',
+                       'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg',
+                       'http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg'
+                     ]}
+                  />
                 </div>
-              //   <div>
-              //     <img src="http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg" />
-              //     <p className="legend">Caption 2</p>
-              //   </div>
-              //   <div>
-              //     <img src="http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg" />
-              //     <p className="legend">Caption 3</p>
-              //   </div>
-              //   <div>
-              //     <img src="http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg" />
-              //     <p className="legend">Caption 4</p>
-              //   </div>
-              //   <div>
-              //     <img src="http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg" />
-              //     <p className="legend">Caption 5</p>
-              //   </div>
-              // </Carousel>
+
+
+
 
         );
     }
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -22,39 +22,42 @@ class Profile extends Component {
     }
 
     componentDidMount() {
-        /* Here is where a call for the persons profile needs to be made. Once the verification is made the credentials will be stored in the 
-        state. The credentials will be passed down as props to all the other components so that when a component loads the information will 
+        /* Here is where a call for the persons profile needs to be made. Once the verification is made the credentials will be stored in the
+        state. The credentials will be passed down as props to all the other components so that when a component loads the information will
         be much faster to show up. */
         console.log(this.state)
         console.log(this.props)
     }
 
-    
+
     render(){
         return(
             <div>
-                <div>Welcome: {this.state.user.name}</div>
                 <Router>
                     <div className="row">
-                        <div className="col s12 m6">
-                            <div className="card blue-grey darken-1">
+                      <div className="col s12 m6">
+                          <div className="card">
                             <div className="card-content white-text">
-                                <span className="card-title">Your Profile</span>
-                                <p>Welcome to your Wym profile {this.state.user.name}!</p>
+                                <img src="http://images.archant.co.uk/polopoly_fs/1.4371969.1452244455!/image/image.jpg_gen/derivatives/landscape_630/image.jpg" className="circle responsive-img" height="100" width="100" />
+                                <span className="card-title">Welcome to your WYM Profile, {this.state.user.name}!</span>
+
                             </div>
-                            <div className="card-action">
+                            <div className="card-action profileLinks">
                                 <Link to ='/history'>Saved Wyms</Link>
                                 <Link to ='/twitterprofile'>Twitter</Link>
                                 <Link to ='/'>Upload new text</Link>
                             </div>
-                            </div>
-                            </div>
+                          </div>
+                        </div>
                         <Route path = '/history' component = { History }/>
                         <Route path = '/twitterprofile' component = { TwitterProfile } />
                     </div>
                 </Router>
+              <div>
                 <WatsonData />
-            </div>
+                </div>
+
+          </div>
         )
     }
 }
