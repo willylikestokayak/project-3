c2bc98176ee401b82e622dc6f87c4371063b805b
willylikestokayak <wccoker@gmail.com>
2017-10-29 13:14:43 -0700

    chart rendering correct data alignment

--- client/src/Entry.js
+++ client/src/Entry.js
@@ -29,9 +29,6 @@ class Entry extends Component {
 
 		}
 	}
-	// this.setState{
-	// 	document_tone.tones[0].tone_id: document_tones.tones[0].score
-	// }
 
 	onChange(e) {
 		var text = e.target.value
@@ -39,18 +36,14 @@ class Entry extends Component {
 			entry: text,
 		})
     }
-    //two functions for saving data to textdb
+
     onSubmit(e) {
-        console.log("SUBMIT BUTTON CLICKED")
         var savedTitle = e.target.value
         this.setState({
             title: savedTitle
         })
     }
     clickSave(e) {
-        console.log("CLICK SAVE");
-        console.log(this.props)
-        console.log(this.state)
         axios.post('/watson/save', {
             user: this.props.user,
             title: this.state.title,
@@ -75,24 +68,7 @@ class Entry extends Component {
 	grabCache() {
 		axios.get('/watson')
 		.then( (response) => {
-			//Reference to link objects
-			console.log(response.data.text)
-			// console.log(response.data.text.document_tone.tones[0].tone_id)
-			// console.log(response.data.text.document_tone.tones[0].score)
-			//var text is essentially the key that I passed in the axios.post call, this is purely for simplicity purposes. 
 			var text = response.data.text
-			// var response0 = response.data.text.document_tone.tones[0].score
-			// var response1 = response.data.text.document_tone.tones[1].score
-			// var response2 = response.data.text.document_tone.tones[2].score
-			// var response3 = response.data.text.document_tone.tones[3].score
-			// var response4 = response.data.text.document_tone.tones[4].score
-			// var response5 = response.data.text.document_tone.tones[5].score
-			// var response6 = response.data.text.document_tone.tones[6].score
-
-
-			/* Some text may not get a document tone, so it is important to verify that there is a tone being sent, if there isn't and
-			there is an empty array, this may cause problems... */
-
 			var tones;
 			if(text) {
 			 	tones = text.document_tone.tones
@@ -107,68 +83,44 @@ class Entry extends Component {
 				tones: tones,
 				sentences: sentences,
 			});
-			// if(response.data.text.document_tone.tones[0].tone_id === "anger"){
-			// 	this.setState({
-			// 		anger: response0
-			// 	})
-			// }
-			// if(response.data.text.document_tone.tones[1].tone_id === "tentative"){
-			// 	this.setState({
-			// 		tentative: response1
-			// 	})
-			// }
-			// if(response.data.text.document_tone.tones[4].tone_id === "tentative"){
-			// 	this.setState({
-			// 		tentative: response1
-			// 	})
-			// }
+
 			for (var i=0; i< (response.data.text.document_tone.tones).length; i++){
 				if(response.data.text.document_tone.tones[i].tone_id === "anger"){
 					this.setState({
 						anger: response.data.text.document_tone.tones[i].score
 					});
-					console.log(this.state.anger)
 				}
 				if(response.data.text.document_tone.tones[i].tone_id === "tentative"){
 					this.setState({
 						tentative: response.data.text.document_tone.tones[i].score
 					});
-					console.log(this.state.tentative)
 				}
 				if(response.data.text.document_tone.tones[i].tone_id === "fear"){
 					this.setState({
 						fear: response.data.text.document_tone.tones[i].score
 					});
-					console.log(this.state.fear)
 				}
 				if(response.data.text.document_tone.tones[i].tone_id === "joy"){
 					this.setState({
 						joy: response.data.text.document_tone.tones[i].score
 					});
-					console.log(this.state.joy)
 				}
 				if(response.data.text.document_tone.tones[i].tone_id === "sadness"){
 					this.setState({
 						sadness: response.data.text.document_tone.tones[i].score
 					});
-					console.log(this.state.sadness)
 				}
 				if(response.data.text.document_tone.tones[i].tone_id === "analytical"){
 					this.setState({
 						analytical: response.data.text.document_tone.tones[i].score
 					});
-					console.log(this.state.analytical)
 				}
 				if(response.data.text.document_tone.tones[i].tone_id === "confident"){
 					this.setState({
 						confident: response.data.text.document_tone.tones[i].score
 					});
-					console.log(this.state.confident)
 				}
 			}
-
-
-
 
 			var name = this.state.tones.map( (item, index) => (item.tone_name) )
         	var score = this.state.tones.map( (item, index) => (item.score) )
@@ -179,8 +131,6 @@ class Entry extends Component {
         	})
 
 		});
-		
-		//this.sentenceClassify()
 	}
 
 
@@ -213,7 +163,7 @@ class Entry extends Component {
                 	</div>
                 </div>
                 <div id='response-component'>
-                	<Response test='test' name={this.state.name} data={this.state.data} analyzed={this.state.analyzed} tentative={this.state.tentative} anger={this.state.anger} joy={this.state.joy} fear={this.state.fear} sadness={this.state.sadness} analytical={this.state.analytical} confident={this.state.confident}/>
+                	<Response name={this.state.name} data={this.state.data} analyzed={this.state.analyzed} tentative={this.state.tentative} anger={this.state.anger} joy={this.state.joy} fear={this.state.fear} sadness={this.state.sadness} analytical={this.state.analytical} confident={this.state.confident} />
                 </div>
             </div>
         );
--- client/src/Response.js
+++ client/src/Response.js
@@ -6,16 +6,6 @@ class Response extends Component {
 
     constructor(props){
         super(props);
-        console.log(this.props.data)
-        // this.state ={
-        //     anger: anger,
-		// 	tentative: tentative,
-		// 	joy: joy,
-		// 	fear: fear,
-		// 	sadness: sadness,
-		// 	analytical: analytical,
-		// 	confident: confident
-        // }
     }
 
 
@@ -23,8 +13,7 @@ class Response extends Component {
         var tentative = this.props.tentative;
     	if(this.props.analyzed) {
 
-    	var name = this.props.name;
-        var data = this.props.data;
+        var name = this.props.name;
         var anger = this.props.anger;
         var tentative = this.props.tentative;
         var joy = this.props.joy;
@@ -32,36 +21,31 @@ class Response extends Component {
         var sadness = this.props.sadness;
         var analytical = this.props.analytical;
         var confident = this.props.confident;
-        //var documentLabel = this.props.document_tone.tone_id;
 
-        
-        // const labels = ['Anger', 'Fear', 'Joy', 'Sadness', 'Analytical', 'Confident', 'Tentative'];
-        // function getIndex(labels, data){
-        //{this.state.tentative}, {this.state.anger}, {this.state.joy}, {this.state.fear}, {this.state.sadness}, {this.state.analytical}, {this.state.confident}
-        //{tentative}, {anger}, {joy}, {fear}, {sadness}, {analytical}, {confident}
-        // }
-///set labels to object state.
     	const data = {
-		  labels:  ['anger', 'sadness', 'fear', 'tentative', 'joy', 'analytical', 'confident'], //[anger, sadness, fear, tentative, joy, analytical, confident],
+		  labels:  ['anger', 'tentative', 'joy', 'fear', 'sadness', 'analytical', 'confident'], //[anger, sadness, fear, tentative, joy, analytical, confident],
 		  datasets: [
 		    {
-		      label: [data],   //[name],    //[anger, tentative, joy, fear, sadness, analytical, confident], //{response.data.text.document_tone.tones[i].tone_id},
+		      label: [anger, tentative, joy, fear, sadness, analytical, confident],   //[data],   //[name],    //, //{response.data.text.document_tone.tones[i].tone_id},
 		      backgroundColor: 'white',
 		      borderColor: 'red',
 		      pointBackgroundColor: 'yellow',
 		      pointBorderColor: '#fff',
 		      pointHoverBackgroundColor: '#fff',
 		      pointHoverBorderColor: 'green',
-		      data: data
+		      data: [anger, tentative, joy, fear, sadness, analytical, confident]
 		}]}
 
         return(
         <div>
             <div className="chart">
-            <Line
+            <Bar
                 data={data} 
-                options={{
-                    title:{
+                options={{}}
+            /> 
+            {/* Tentative is equal to {this.props.tentative} */}
+
+            {/* title:{
                         display: this.props.displayTitle,
                         text: 'Watson is checking your words',
                         fontSize: 25
@@ -72,11 +56,7 @@ class Response extends Component {
                     },
                     labels:{
                         fontSize: 25,
-                        backgroundColor: '#76e7cd'
-                    }
-                }}
-            /> 
-            {/* Tentative is equal to {this.props.tentative} */}
+                        backgroundColor: '#76e7cd' */}
             </div>
         </div>
         );
